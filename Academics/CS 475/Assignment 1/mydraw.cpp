c3146d92d0ddd1a0672d27d9031603b1327356b8
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-10-23 20:36:40 +0530

    Fixing carry, zero logic, LM mistake

--- Academics/CS 475/Assignment 1/mydraw.cpp
+++ Academics/CS 475/Assignment 1/mydraw.cpp
@@ -1,6 +1,6 @@
 /*******************************
  *
- * CS475m Assignment 1 Basecode 
+ * CS475m Assignment 1 Basecode
  *
  * Parag Chaudhuri, 2016
  *
@@ -14,18 +14,22 @@
 #include "mydraw_class.hpp"
 
 //Window width
-int win_width = 512;
+int win_width = 1024;
 //Window height
-int win_height = 512;
-
-
+int win_height = 640;
+canvas_t* canvas;
+int LINE_DRAWING = 0;
+int TRI_DRAWING = 2;
+int mode = 1;
+int FILL_DRAWING = 3;
+int point1[2] = {-1, -1};
+int point2[2] = {-1, -1};
+int point3[2] = {-1, -1};
 
 //Display callback
 void display( void )
 {
-  //This clears the colorbuffer (current window)
-  glClear(GL_COLOR_BUFFER_BIT);
-
+  canvas->draw_grid();
   //Flush the framebuffer to the screen
   glutSwapBuffers();
 }
@@ -49,41 +53,155 @@ void reshape( int w, int h )
 
 //Keyboard callback
 void keyboard( unsigned char key, int x, int y ) {
-  switch(key) {
-  //Exit on pressing escape
-  case 27: 
-    exit(0);
-    break;
-    //Do something when 'C' is pressed
-  case 'C':
-    break;
+    switch(key) {
+    //Exit on pressing escape
+    case 27:
+        exit(0);
+        break;
+    case '1': {
+        mode = LINE_DRAWING;
+    } break;
+    case '2': {
+        mode = TRI_DRAWING;
+    } break;
+    case 'f': {
+        mode = FILL_DRAWING;
+    } break;
+    case 'c': {
+        canvas->clear(true);
+    } break;
+    case 'e': {
+        canvas->pen->toggle_eraser();
+    } break;
+    case 'g': {
+        cout << "Enter fill color. Each channel from 0 to 1" << endl;
+        float r, g, b;
+        cin >> r >> g >> b;
+        color_t* color1 = new color_t(r, g, b);
+        canvas->fill->set_fill_color(color1);
+    } break;
+    case 'h': {
+        cout << "Enter pen color. Each channel from 0 to 1" << endl;
+        float r, g, b;
+        cin >> r >> g >> b;
+        color_t* color1 = new color_t(r, g, b);
+        canvas->pen->set_color(color1);
+    } break;
+    case 'i': {
+        cout << "Enter background color" << endl;
+        float r, g, b;
+        cin >> r >> g >> b;
+        color_t* color1 = new color_t(r, g, b);
+        canvas->set_background(color1, true);
+    } break;
+    case 'j': {
+        cout << "Enter pen size" << endl;
+        int size;
+        cin >> size;
+        canvas->pen->set_size(size);
+    } break;
+    case 'n': {
+        // Do something when 'N' is pressed
+        cout << "Enter the RGB. Each channel from 0 to 1" << endl;
+        float r = 0, g = 0, b = 0;
+        cin >> r >> g >> b;
+        color_t* color = new color_t(r, g, b);
+        canvas = new canvas_t(win_width, win_height, color);
+    } break;
+    case 's': {
+        // Do something when 'S' is pressed
+        cout << "Enter file name" << endl;
+        string s = "";
+        cin >> s;
+        canvas->save(s);
+    } break;
+    case 'l': {
+        // Do something when 'l' is pressed
+        cout << "Enter file name" << endl;
+        string s = "";
+        cin >> s;
+        canvas->load(s);
+        canvas->draw_list();
+    } break;
     //Ignore all other keypresses
-  default:
-    break;
-  }
+    }
+    glutPostRedisplay();
 }
 
 //Mouse callback
-void mouse(int button, int state, int x, int y) 
+void mouse(int button, int state, int x, int y) {
+    y = win_height - y;
+    if (state == GLUT_DOWN) {
+        if (button == GLUT_LEFT_BUTTON) {
+            if (mode == LINE_DRAWING) {
+                if (point1[0] == -1 || point1[1] == -1) {
+                    point1[0] = x;
+                    point1[1] = y;
+                } else {
+                    point2[0] = x;
+                    point2[1] = y;
+                    color_t* color = new color_t(0,0,0);
+                    point_t* p1 = new point_t(point1[0], point1[1], color);
+                    point_t* p2 = new point_t(point2[0], point2[1], color);
+                    line_t* l1 = new line_t(p1, p2, color);
+                    l1->draw(canvas, false);
+                    point1[0] = -1;
+                    point1[1] = -1;
+                    point2[0] = -1;
+                    point2[1] = -1;
+                }
+            } else if(mode == TRI_DRAWING) {
+                if (point1[0] == -1 || point1[1] == -1) {
+                    point1[0] = x;
+                    point1[1] = y;
+                } else if(point2[0] == -1 || point2[1] == -1) {
+                    point2[0] = x;
+                    point2[1] = y;
+                } else {
+                    point3[0] = x;
+                    point3[1] = y;
+                    color_t* color = new color_t(0,0,0);
+                    point_t* p1 = new point_t(point1[0], point1[1], color);
+                    point_t* p2 = new point_t(point2[0], point2[1], color);
+                    point_t* p3 = new point_t(point3[0], point3[1], color);
+                    triangle_t* t1 = new triangle_t(p1, p2, p3, color);
+                    t1->draw(canvas, false);
+                    point1[0] = -1;
+                    point1[1] = -1;
+                    point2[0] = -1;
+                    point2[1] = -1;
+                    point3[0] = -1;
+                    point3[1] = -1;
+                }
+            } else if(mode == FILL_DRAWING) {
+                canvas->fill->draw(canvas, x, y, false);
+            }
+        } else if (button == GLUT_RIGHT_BUTTON) {
+            if (mode == LINE_DRAWING) {
+                if (point2[0] == -1 || point2[1] == -1) {
+                    point1[0] = -1;
+                    point1[1] = -1;
+                }
+            } else if (mode == TRI_DRAWING) {
+                if (point2[0] == -1 || point2[1] == -1) {
+                    point1[0] = -1;
+                    point1[1] = -1;
+                } else if(point3[0] == -1 || point3[1] == -1) {
+                    point2[0] = -1;
+                    point2[1] = -1;
+                }
+            }
+        }
+    }
+    glutPostRedisplay();
+}
+
+
+int main (int argc, char *argv[])
 {
-   if (state == GLUT_DOWN) 
-     {
-       if (button == GLUT_LEFT_BUTTON) 
-	 {
-	   //Do something when the left mouse button is clicked
-	 }
-     }
-   glutPostRedisplay();
-}       
-
-
-int main (int argc, char *argv[]) 
-{
-
   glutInit( &argc, argv );
   glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
   glutInitWindowSize( win_width, win_height );
-
   //Open a GLUT window
   glutCreateWindow( "MyDraw" );
 
@@ -91,6 +209,7 @@ int main (int argc, char *argv[])
   glutReshapeFunc( reshape );
   glutKeyboardFunc( keyboard );
   glutMouseFunc( mouse );
- 
+  color_t* color = new color_t(0, 0, 0);
+  canvas = new canvas_t(win_width, win_height, color);
   glutMainLoop();
 }
--- Academics/CS 475/Assignment 1/mydraw_class.hpp
+++ Academics/CS 475/Assignment 1/mydraw_class.hpp
@@ -1,11 +1,18 @@
 #ifndef _MYDRAW_CLASS_HPP_
 #define _MYDRAW_CLASS_HPP_
-
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
 //Define all classes like the color class, adding appropriate methods and data members. 
 //Implementation of the methods go into the corresponding cpp file
 
 //------------------------
 //color_t class
+class canvas_t;
+class drawing_t;
+class pen_t;
+class fill_t;
 
 class color_t {
 private:
@@ -25,16 +32,136 @@ public:
 //point_t class
 class point_t {
 private:
-	float x,y;
-	color_t color;
+	int x,y;
+	color_t* color;
 public:
 	point_t();
-	float get_x(void);
-	float get_y(void);
-	float get_color(void);
-	float draw(void);
+	point_t(int _x, int _y, color_t* c);
+	int get_x(void);
+	int get_y(void);
+	color_t* get_color(void);
+	void draw(canvas_t* canvas, bool saved);
+	void from_string(string input);
+	string to_string();
+	void set_color(color_t* c);
 };
 
 //------------------------
+
+//canvas_t class
+class canvas_t {
+private:
+	int width;
+	int height;
+	vector< vector<point_t*> > image;
+	color_t* background;
+	drawing_t* drawing;
+public:
+	pen_t* pen;
+	fill_t* fill;
+	canvas_t(int width, int height, color_t* background);
+	void clear(bool put_config);
+	void put_point(point_t* point);
+	void draw_grid(void);
+	void append(char op, string input);
+	void save(string file_name);
+	void load(string file_name);
+	void draw_list(void);
+	point_t* get_point(int x, int y);
+	void set_point_color(int x, int y, color_t* color);
+	int get_height();
+	int get_width();
+	void set_background(color_t* color, bool save);
+	color_t* get_background();
+	void background_from_string(string input);
+	string background_to_string();
+
+};
+
+//line_t class
+class line_t {
+private:
+	point_t* p1;
+	point_t* p2;
+	color_t* color;
+public:
+	line_t();
+	line_t(point_t* p1, point_t* p2, color_t* color);
+	point_t* get_p1();
+	point_t* get_p2();
+	color_t* get_color();
+	void draw(canvas_t* canvas, bool saved);
+	void from_string(string input);
+	string to_string();
+
+};
+
+//triangle_t class
+class triangle_t {
+private:
+	point_t* p1;
+	point_t* p2;
+	point_t* p3;
+	color_t* color;
+public:
+	triangle_t();
+	triangle_t(point_t* p1, point_t* p2, point_t* p3, color_t* color);
+	point_t* get_p1();
+	point_t* get_p2();
+	point_t* get_p3();
+	color_t* get_color();
+	void draw(canvas_t* canvas, bool saved);
+	void from_string(string input);
+	string to_string();
+};
+
+
+
+//drawing_t class
+class drawing_t {
+private:
+	list<char> operations;
+	list<string> inputs;
+public:
+	drawing_t();
+	void load(string file_name);
+	void save(string file_name);
+	void append(char op, string input);
+	void draw(canvas_t* canvas);
+	void clear();
+};
+
+//pen_t class
+class pen_t {
+private:
+	int size;
+	color_t* color;
+	bool eraser;
+	canvas_t* canvas;
+public:
+	pen_t(canvas_t* canvas);
+	pen_t(canvas_t* canvas, int size, color_t* color, bool eraser);
+	int get_size(void);
+	color_t* get_color(void);
+	bool get_eraser();
+	void set_size(int size);
+	void set_color(color_t* color);
+	void toggle_eraser();
+	void from_string(string input);
+	string to_string();
+};
+
+//fill_t class
+class fill_t {
+private:
+	color_t* fill_color;
+public:
+	fill_t(color_t* fill_color);
+	void set_fill_color(color_t* color);
+	color_t* get_fill_color(void);
+	void draw(canvas_t* canvas, int x, int y, bool from_string);
+	void from_string(string input, canvas_t* canvas);
+	string to_string(int x, int y);
+};
 
 #endif
